a0e4e2378b2182d5afa5cec411fce5d4a1898a7d
Nick <33908215+NicholasG04@users.noreply.github.com>
2021-01-05 23:54:00 +0000

    Added botbuilder framework

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import latex from './routes/latex';
 import { json } from 'body-parser';
 import helmet from 'helmet';
+require('dotenv').config();
 
 const app = express();
 const port = 3987;
@@ -15,3 +16,6 @@ app.post('/latex', latex);
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
 });
+
+
+  // joHcpGMRXur6+bfwx/BV1yKRsCAliyrELWJtFinO46k=
--- src/routes/latex.ts
+++ src/routes/latex.ts
@@ -1,9 +1,17 @@
 import crypto from 'crypto';
 import type express from 'express'
+const botbuilder = require('botbuilder');
 
 export default (req: express.Request, res: express.Response) => {
-  const { body } = req;
-  console.log(body)
-  res.statusCode = 200;
-  res.end('success')
+  const adapter = new botbuilder.BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+  })
+
+  adapter.processActivity(req, res, async (turnContext: any) => {
+    if (turnContext.activity.type === 'message') {
+      const msg = turnContext.activity.text;
+      await turnContext.sendActivity(`Echo: ${msg}`)
+    }
+  })
 }
